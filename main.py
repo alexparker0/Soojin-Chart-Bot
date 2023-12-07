1fccfbe59204b5ecc6a334e541084d58bf8171fe
alexparker0 <152001885+alexparker0@users.noreply.github.com>
2023-12-07 12:23:57 +0530

    Port to py-cord

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from melonapi import scrapeMelon
 import discord
 from discord.ext import commands
-from discord import app_commands
+#from discord import app_commands
 import os
 import json
 
-class MyClient(discord.Client):
-  def __init__(self, *, intents: discord.Intents):
-    super().__init__(intents=intents)
+#MY_GUILD = discord.Object(id=1176471349896683550)
+#class MyClient(discord.Client):
+ # def __init__(self, *, intents: discord.Intents):
+  #  super().__init__(intents=intents)
     # A CommandTree is a special type that holds all the application command
     # state required to make it work. This is a separate class because it
     # allows all the extra state to be opt-in.
@@ -15,22 +16,20 @@ class MyClient(discord.Client):
     # to store and work with them.
     # Note: When using commands.Bot instead of discord.Client, the bot will
     # maintain its own tree instead.
-    self.tree = app_commands.CommandTree(self)
+    #self.tree = app_commands.CommandTree(self)
 
-  async def setup_hook(self):
-    # This copies the global commands over to your guild.
-    self.tree.copy_global_to(guild=MY_GUILD)
-    await self.tree.sync(guild=MY_GUILD)
-    
-MY_GUILD = discord.Object(id=guild_id)
 intents = discord.Intents.default()
-client = MyClient(intents=intents)
-@client.event
-async def on_guild_join(guild):
-  guild_id = guild.id
+bot = commands.Bot("!", intents=intents)
 
+@bot.event
+async def on_ready():
+  print("Get ready to kpop!")
 
-
+  
+#async def setup_hook(self):
+    # This copies the global commands over to your guild.
+   # self.tree.copy_global_to(guild=MY_GUILD)
+    #await self.tree.sync(guild=MY_GUILD)
 
 
 
@@ -41,14 +40,11 @@ def truncate_string(original_string, max_length):
   else:
       return original_string
 
-@client.event
-async def on_ready():
-  print("Get ready to kpop!")
+
   
-@client.tree.command()
-async def chart_live(interaction: discord.Interaction):
-  """Gives the live Melon chart!"""
-  await interaction.response.send_message("Loading your data...")
+@bot.slash_command(name="live_chart", description="Gives the live Melon chart!")
+async def live_chart(ctx):
+  await ctx.respond("Loading your data...")
   live_chart = scrapeMelon.getList("LIVE").decode()
   data = json.loads(live_chart)
   print(data)
@@ -66,16 +62,15 @@ async def chart_live(interaction: discord.Interaction):
 
           embed.add_field(name=f"{ranking}. {name}", value=f"Artists: {artists}", inline=False)
 
-  await interaction.followup.send(embed=embed)  
+  await ctx.followup.send(embed=embed)  
   
-@client.tree.command()
-async def chart_day(interaction: discord.Interaction):
-  """Gives the Melon chart for the day!"""
-  await interaction.response.send_message("Loading your data...")
+@bot.slash_command(name="daily_chart", description="Gives the Melon chart of the day!")
+async def daily_chart(ctx):
+  await ctx.respond("Loading your data...")
   day_chart = scrapeMelon.getList("DAY").decode()
   data = json.loads(day_chart)
   print(data)
-  embed = discord.Embed(title="Top 10 Songs - Daily Melon Chart", color=discord.Color.red())
+  embed = discord.Embed(title="Top 10 Songs - Daily Melon Chart")
 
   for i in range(1, 26):
       item_key = str(i)
@@ -88,16 +83,15 @@ async def chart_day(interaction: discord.Interaction):
 
           embed.add_field(name=f"{ranking}. {name}", value=f"Artists: {artists}", inline=False)
 
-  await interaction.followup.send(embed=embed)
+  await ctx.followup.send(embed=embed)
 
-@client.tree.command()
-async def chart_week(interaction: discord.Interaction):
-  """Gives the Melon chart for the week!"""
-  await interaction.response.send_message("Loading your data...")
+@bot.slash_command(name="weekly_chart", description="Gives the Melon chart of the week!")
+async def weekly_chart(ctx):
+  await ctx.respond("Loading your data...")
   week_chart = scrapeMelon.getList("WEEK").decode()
   data = json.loads(week_chart)
   print(data)
-  embed = discord.Embed(title="Top 10 Songs - Weekly Melon Chart", color=discord.Color.red())
+  embed = discord.Embed(title="Top 10 Songs - Weekly Melon Chart")
 
   for i in range(1, 26):
       item_key = str(i)
@@ -110,16 +104,15 @@ async def chart_week(interaction: discord.Interaction):
   
           embed.add_field(name=f"{ranking}. {name}", value=f"Artists: {artists}", inline=False)
   
-  await interaction.followup.send(embed=embed)
+  await ctx.followup.send(embed=embed)
 
-@client.tree.command()
-async def chart_month(interaction: discord.Interaction):
-  """Gives the Melon chart for the month!"""
-  await interaction.response.send_message("Loading your data...")
+@bot.slash_command(name="monthly_chart", description="Gives the Melon chart of the month!")
+async def monthly_chart(ctx):
+  await ctx.respond("Loading your data...")
   month_chart = scrapeMelon.getList("MONTH").decode()
   data = json.loads(month_chart)
   print(data)
-  embed = discord.Embed(title="Top 10 Songs - Monthly Melon Chart", color=discord.Color.red())
+  embed = discord.Embed(title="Top 10 Songs - Monthly Melon Chart")
   for i in range(1, 26):
       item_key = str(i)
       item_data = data.get(item_key)
@@ -131,15 +124,12 @@ async def chart_month(interaction: discord.Interaction):
 
           embed.add_field(name=f"{ranking}. {name}", value=f"Artists: {artists}", inline=False)
 
-  await interaction.followup.send(embed=embed)
+  await ctx.followup.send(embed=embed)
 
-@client.tree.command()
-@app_commands.describe(
-  song_name="The name of the song you want the lyrics of"
-)
-async def search_lyrics(interaction: discord.Interaction, song_name: str):
+@bot.slash_command(name="search_lyrics", description="Gives the lyrics of a song!")
+async def search_lyrics(ctx, song_name: str):
   """Searches for the lyrics of a song!"""
-  await interaction.response.send_message("Loading your data...")
+  await ctx.respond("Loading your data...")
   song_list = scrapeMelon.getList("LIVE").decode()
   data = json.loads(song_list)
   matching_item = next((item for item in data.values() if song_name.lower() in item.get("name", "").lower()), None)
@@ -149,6 +139,6 @@ async def search_lyrics(interaction: discord.Interaction, song_name: str):
     print(lyrics)
     send_lyrics = truncate_string(lyrics, 1990)
     print(send_lyrics)
-    await interaction.followup.send(send_lyrics)
+    await ctx.followup.send(send_lyrics)
 
-client.run("MTE3NjQ3MTU0ODM3OTU0OTc5Ng.GRUbG9.oC4n2uf-2CFD2k7jxeJ62Q3Jv7ysXB_Boop4Mw")
+bot.run("MTE3NjQ3MTU0ODM3OTU0OTc5Ng.GRUbG9.oC4n2uf-2CFD2k7jxeJ62Q3Jv7ysXB_Boop4Mw")
